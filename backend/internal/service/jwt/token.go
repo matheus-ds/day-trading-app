d5016dffa10fa713858b64b2a04d2a6893698a19
Jackson <94677564+jluover9000@users.noreply.github.com>
2024-02-20 13:09:00 -0800

    Add JWTManager for JWT token generation
    adds a new folder which contain file jwt.go that contains the JWTManager struct and methods
    for generating JWT tokens with the provided secret key and expiration duration.

---
+++ backend/internal/service/jwt/token.go
@@ -0,0 +1,40 @@
+package token
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTManager struct {
+	secretKey     string
+	tokenDuration time.Duration
+}
+
+// NewJWTManager creates a new instance of JWTManager
+func NewJWTManager(secretKey string, tokenDuration time.Duration) *JWTManager {
+	return &JWTManager{
+		secretKey:     secretKey,
+		tokenDuration: tokenDuration,
+	}
+}
+
+// GenerateToken generates a new JWT token
+func (jm *JWTManager) GenerateToken(userID int64) (string, error) {
+	// Define the token claims
+	claims := jwt.MapClaims{
+		"user_id": userID,                                  // Include any additional claims you need
+		"exp":     time.Now().Add(jm.tokenDuration).Unix(), // Token expiration time
+	}
+
+	// Create a new token with the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign the token with the secret key and get the complete encoded token as a string
+	tokenString, err := token.SignedString([]byte(jm.secretKey))
+	if err != nil {
+		return "", err // Error generating token
+	}
+
+	return tokenString, nil
+}
