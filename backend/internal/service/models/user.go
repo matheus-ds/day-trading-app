2d70e4fcc03f8fe9a098724d679bcbb30ba1eafb
Jackson <94677564+jluover9000@users.noreply.github.com>
2024-02-24 19:15:33 -0800

    clean up models\user.go

--- backend/internal/service/models/user.go
+++ backend/internal/service/models/user.go
@@ -1,11 +1,6 @@
 package models
 
-import (
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type User struct {
-    UserName     string      `json:"userName"      bson:"user_name" `       
-    PasswordHash string      ` json:"passwordHash" bson:"password_hash"`
+	UserName     string `json:"userName"      bson:"user_name" `
+	PasswordHash string ` json:"passwordHash" bson:"password_hash"`
 }
