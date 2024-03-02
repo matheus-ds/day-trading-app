efc4ae852fa08ef1fe23dd177417155a46b50c8a
TaylorLewis <taylorlewis@uvic.ca>
2024-03-01 18:13:58 -0800

    matching/execute: Make basic template

---
+++ backend/internal/service/matching/execute.go
@@ -0,0 +1,61 @@
+package matching
+
+import "day-trading-app/backend/internal/service/models"
+
+func ExecuteOrders(stockTxCommitQueue []models.StockMatch) {
+	for _, tx := range stockTxCommitQueue {
+		if tx.Order.IsBuy {
+			executeBuy(tx)
+		} else {
+			executeSell(tx)
+		}
+	}
+}
+
+func executeBuy(tx models.StockMatch) {
+	if tx.Order.OrderStatus == "IN_PROGRESS" { // unfulfilled
+		// Add deducted money back to wallet
+
+		// Delete stock transaction
+
+		// Delete wallet transaction
+
+	} else if tx.Order.OrderStatus == "PARTIALLY_FULFILLED" {
+		// Refund remaining wallet amount
+
+		// Delete wallet transaction
+
+	} else if tx.Order.OrderStatus == "COMPLETED" {
+		// Add stock quantity to user portfolio
+
+		// Update stock transaction status to completed
+
+	} else if tx.Order.OrderStatus == "" {
+		// Error: empty string
+	} else {
+		// Error: spelling probably
+	}
+}
+
+func executeSell(tx models.StockMatch) {
+	if tx.Order.OrderStatus == "IN_PROGRESS" { // unfulfilled
+		// Add stock quantity back to user portfolio
+
+		// Delete stock transaction
+
+	} else if tx.Order.OrderStatus == "PARTIALLY_FULFILLED" {
+		// Add remaining stock quantity back to user portfolio
+
+	} else if tx.Order.OrderStatus == "COMPLETED" {
+		// Update stock transaction to completed
+
+		// Add money to wallet
+
+		// Insert wallet transaction
+
+	} else if tx.Order.OrderStatus == "" {
+		// Error: empty string
+	} else {
+		// Error: spelling probably
+	}
+}
